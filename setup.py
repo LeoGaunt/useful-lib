be85b0ee052372881a3f66cdfcf208718acc011d
Leo Gaunt <leogaunt05@outlook.com>
2020-09-04 18:48:10 +0100

    Added lbl and fixed indentation

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='useful-lib',
-     version='0.1',
-     scripts=['useful-lib'] ,
-     author="Leo Gaunt",
-     author_email="leogaunt05@outlook.com",
-     description="A package containing scripts to help boost your python",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/LeoGaunt/useful-lib",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+    name='useful-lib',
+    version='0.1',
+    scripts=['useful-lib'] ,
+    author="Leo Gaunt",
+    author_email="leogaunt05@outlook.com",
+    description="A package containing scripts to help boost your python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeoGaunt/useful-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
